eb276eda4759ea4dc06e0542ce08de5fa6d0aeb7
VanDarakHome <150126102+VanDarakHome@users.noreply.github.com>
2025-12-03 20:05:14 +0300

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,69 +1,59 @@
 import json
+from bosses import bosses1
+with open("locations.json", encoding="utf-8") as file:
+    location = json.load(file)
 
+current_location = 'start'
 
-
-class GameEngine:
-
+while "ending" not in current_location:
     
-    def __init__(self, map_file='locations.json'):
-        self.load_map(map_file)
-        self.current_location = 'start'
-        self.visited_locations = ['start']
+    current_location_info = location[current_location]
+    print(current_location_info["lore"])
 
-    
-    def load_map(self, map_file):
-        with open(map_file, 'r', encoding='utf-8') as f:
-            self.game_map = json.load(f)
-
-    
-    def get_current_location_info(self):
-        location = self.game_map[self.current_location]
-        return {
-            'id': location['id'],
-            'name': location['name'],
-            'next': location.get('next', []),
-            'is_finish': 'finish' in location,
-            'finish_type': location.get('finish')
-        }
-
-    
-    def get_available_moves(self):
-        current_info = self.get_current_location_info()
-        spisok = [] 
-        for i in current_info['next']:
-            location = self.game_map[i]
-            spisok.append({
-                'id': i,
-                'name': location['name']
-            }) 
-        return spisok
-
-    
-    def move_to(self, location_id):
-        current_info = self.get_current_location_info()
-        if location_id in current_info['next']:
-            self.current_location = location_id
-            if location_id not in self.visited_locations:
-                self.visited_locations.append(location_id)
-            return True
+    if current_location_info["id"] == "boss1":
+        if bosses1.boss1():
+            current_location = current_location_info["next_good"]
         else:
-            return False
+            current_location = current_location_info["next_bad"]
+    elif current_location_info["id"] == "boss2":
+        if bosses1.boss2():
+            current_location = current_location_info["next_good"]
+        else:
+            current_location = current_location_info["next_bad"]
+    elif current_location_info["id"] == "patriot_test":
+        result = bosses1.test()
+        if result == 0:
+            current_location = current_location_info["next_bad"]
+        elif result == 1:
+            current_location = current_location_info["next_neutral"]
+        else:
+            current_location = current_location_info["next_good"]
+    elif current_location_info["id"] == "location2":
+        print(current_location_info["question"])
+        print(*current_location_info["answers"], sep="\n")
+        vvod = input()
+        while vvod != "1" and vvod != "2" and vvod != "3":
+            print("Введите число от 1 до 3")
+            vvod = input()
+        vvod = int(vvod)
+        if vvod == 1:
+            current_location = current_location_info["next_bad"]
+        elif vvod == 2:
+            current_location = current_location_info["next_good"]
+        else:
+            current_location = current_location_info["next_neutral"]
+    else:
+        print(current_location_info["question"])
+        print(*current_location_info["answers"], sep="\n")
 
-    
-    def is_game_finished(self):
-        return self.get_current_location_info()['is_finish']
+        vvod = input()
+        while vvod != "1" and vvod != "2":
+            print("Введите число 1 или 2")
+            vvod = input()
 
-    
-    def get_game_result(self):
-        if self.is_game_finished():
-            return self.get_current_location_info()['finish_type']
-        return None
-
-    
-    def get_visited_locations(self):
-        return [self.game_map[i]['name'] for i in self.visited_locations]
-
-    
-    def reset_game(self):
-        self.current_location = 'start'
-        self.visited_locations = ['start']
+        vvod = int(vvod)
+        if vvod == current_location_info["answer"]:
+            current_location = current_location_info["next_good"]
+        else:
+            current_location = current_location_info["next_bad"]
+print(location[current_location]["lore"])
